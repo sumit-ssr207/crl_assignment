54214b7d7d26486d34353160b14193cac7874bf2
Sumit <sumit.ssr207@gmail.com>
2022-09-01 08:42:03 +0530

    Removed authentication from public informative pages

--- user_listing/urls.py
+++ user_listing/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('get_filtered_data_api', login_required(views.get_filtered_data_api), name='get_filtered_data_api'),
     path('single_user_info/<str:userid>/', login_required(views.single_user_info), name='single_user_info'),
     path('fetch_large_random_data_for_load_test', login_required(views.fetch_large_random_data_for_load_test), name='fetch_large_random_data_for_load_test'),
-    path('api_screen', login_required(views.api_screen), name='api_screen'),
-    path('performance_screen', login_required(views.performance_screen), name='performance_screen'),
+    path('api_screen', views.api_screen, name='api_screen'),
+    path('performance_screen', views.performance_screen, name='performance_screen'),
     
 ]
